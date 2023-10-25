9a06fccfc6c0b2182dba8e1eab3bfca59ae353c0
xany <jgabaextra@gmail.com>
2023-10-25 18:13:57 +0200

    Fog

--- src/App.svelte
+++ src/App.svelte
@@ -1,143 +1,209 @@
 <script>
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import GUI from 'lil-gui/dist/lil-gui.esm.min'; 
+    import * as THREE from "three";
+    import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+    import GUI from "lil-gui/dist/lil-gui.esm.min";
 
-THREE.ColorManagement.enabled = false
+    THREE.ColorManagement.enabled = false;
 
-/**
- * Base
- */
-// Debug
-const gui = new GUI();
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
+    /**
+     * Base
+     */
+    // Debug
+    const gui = new GUI();
+    // Canvas
+    const canvas = document.querySelector("canvas.webgl");
 
-// Scene
-const scene = new THREE.Scene()
+    // Scene
+    const scene = new THREE.Scene();
 
-/**
- * Textures
- */
-const textureLoader = new THREE.TextureLoader()
+    //Fog
+    const fog = new THREE.Fog('#262837', 1, 15)
+    scene.fog = fog
+    /**
+     * Textures
+     */
+    const textureLoader = new THREE.TextureLoader();
 
-/**
- * House
- */
-const house = new THREE.Group()
-scene.add(house);
+    /**
+     * House
+     */
+    const house = new THREE.Group();
+    scene.add(house);
+
+    //Walls
+    const walls = new THREE.Mesh(
+        new THREE.BoxGeometry(6, 2.5, 6),
+        new THREE.MeshStandardMaterial({ color: "#ac8e82" })
+    );
+    walls.position.y = 2.5 / 2;
+    scene.add(walls);
+
+    // Floor
+    const floor = new THREE.Mesh(
+        new THREE.PlaneGeometry(30, 30),
+        new THREE.MeshStandardMaterial({ color: "#a9c388" })
+    );
+    floor.rotation.x = -Math.PI * 0.5;
+    floor.position.y = 0;
+    house.add(floor);
+
+    //Roof
+    const roof = new THREE.Mesh(
+        new THREE.ConeGeometry(5, 1, 4),
+        new THREE.MeshStandardMaterial({ color: "#b35f45" })
+    );
+    roof.position.y = 2.5 + 0.5;
+    roof.rotation.y = Math.PI / 4;
+    house.add(roof);
+
+    //Door
+    const door = new THREE.Mesh(
+        new THREE.PlaneGeometry(2, 2),
+        new THREE.MeshStandardMaterial({ color: "red" })
+    );
+    door.position.z = 3 + 0.01;
+    door.position.y = 2.5 / 2;
+    house.add(door);
+
+    //Bush
+    const bushGeometry = new THREE.SphereGeometry(1, 16, 16);
+    const bushMaterial = new THREE.MeshStandardMaterial({ color: "green" });
+
+    const bush1 = new THREE.Mesh(bushGeometry, bushMaterial);
+    bush1.scale.set(0.5, 0.5, 0.5);
+    bush1.position.set(0.8, 0.2, 3);
+
+    const bush2 = new THREE.Mesh(bushGeometry, bushMaterial);
+    bush2.scale.set(0.3, 0.3, 0.3);
+    bush2.position.set(0.8 * 1.75, 0.1, 3);
+
+    const bush3 = new THREE.Mesh(bushGeometry, bushMaterial);
+    bush3.scale.set(0.5, 0.5, 0.5);
+    bush3.position.set(-0.8, 0.2, 3);
+
+    const bush4 = new THREE.Mesh(bushGeometry, bushMaterial);
+    bush4.scale.set(0.3, 0.3, 0.3);
+    bush4.position.set(-0.8 * 1.75, 0.2, 3);
+
+    const bush5 = new THREE.Mesh(bushGeometry, bushMaterial);
+    bush5.scale.set(0.25, 0.25, 0.25);
+    bush5.position.set(-0.8 * 1.75, 0.15, 3.5);
+    house.add(bush5, bush1, bush2, bush3, bush4);
+
+    //Graves
+    const graves = new THREE.Group();
+    scene.add(graves);
+
+    const graveGeometry = new THREE.BoxGeometry(0.6, 0.8, 0.2);
+    const graveMaterial = new THREE.MeshStandardMaterial({ color: "grey" });
+
+    for (let i = 0; i < 50; i++) {
+        const angle = Math.random()* Math.PI*2
+        const radius = 6 + Math.random()*8
+        const x = Math.sin(angle) * radius
+        const z = Math.cos(angle) * radius
+
+        const grave = new THREE.Mesh(graveGeometry,graveMaterial)
+        grave.position.set(x,0.3,z)
+        grave.rotation.y = Math.random() * 0.5
+        grave.rotation.z = Math.random() * 0.2
+        graves.add(grave);
+    }
+
+    /**
+     * Lights
+     */
+    // Ambient light
+    const ambientLight = new THREE.AmbientLight("#ffffff", .3);
+    gui.add(ambientLight, "intensity").min(0).max(1).step(0.001);
+    scene.add(ambientLight);
+
+    // Directional light
+    const moonLight = new THREE.DirectionalLight("#b9d5ff", .1);
+    moonLight.position.set(4, 5, -2);
+    gui.add(moonLight, "intensity").min(0).max(3).step(0.001);
+    gui.add(moonLight.position, "x").min(-5).max(5).step(0.001);
+    gui.add(moonLight.position, "y").min(-5).max(5).step(0.001);
+    gui.add(moonLight.position, "z").min(-5).max(5).step(0.001);
+    scene.add(moonLight);
 
 
-//Walls
-const walls = new THREE.Mesh(
-    new THREE.BoxGeometry(6,2.5,6),
-    new THREE.MeshStandardMaterial({color: '#ac8e82'})
-)
-walls.position.y = 2.5 / 2
-scene.add(walls);
+    //Door light
+    const doorLight = new THREE.PointLight('#ff7d46', 2.5, 100)
+    doorLight.position.set(0, 2.5, 4)
+    house.add(doorLight);
+    /**
+     * Sizes
+     */
+    const sizes = {
+        width: window.innerWidth,
+        height: window.innerHeight,
+    };
 
-// Floor
-const floor = new THREE.Mesh(
-    new THREE.PlaneGeometry(20, 20),
-    new THREE.MeshStandardMaterial({ color: '#a9c388' })
-)
-floor.rotation.x = - Math.PI * 0.5
-floor.position.y = 0
-scene.add(floor)
+    window.addEventListener("resize", () => {
+        // Update sizes
+        sizes.width = window.innerWidth;
+        sizes.height = window.innerHeight;
 
-//Roof
-const roof = new THREE.Mesh(
-    new THREE.ConeGeometry(5, 1, 4),
-    new THREE.MeshStandardMaterial({color: "#b35f45"})
-)
-roof.position.y = 2.5 + 0.5;
-roof.rotation.y = Math.PI / 4;
-scene.add(roof)
-/**
- * Lights
- */
-// Ambient light
-const ambientLight = new THREE.AmbientLight('#ffffff', 1)
-gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001)
-scene.add(ambientLight)
+        // Update camera
+        camera.aspect = sizes.width / sizes.height;
+        camera.updateProjectionMatrix();
 
-// Directional light
-const moonLight = new THREE.DirectionalLight('#ffffff', 1)
-moonLight.position.set(4, 5, - 2)
-gui.add(moonLight, 'intensity').min(0).max(3).step(0.001)
-gui.add(moonLight.position, 'x').min(- 5).max(5).step(0.001)
-gui.add(moonLight.position, 'y').min(- 5).max(5).step(0.001)
-gui.add(moonLight.position, 'z').min(- 5).max(5).step(0.001)
-scene.add(moonLight)
+        // Update renderer
+        renderer.setSize(sizes.width, sizes.height)
+        renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+       
+    })
 
-/**
- * Sizes
- */
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
+    /**
+     * Camera
+     */
+    // Base camera
+    const camera = new THREE.PerspectiveCamera(
+        75,
+        sizes.width / sizes.height,
+        0.1,
+        100
+    );
+    camera.position.x = 4;
+    camera.position.y = 2;
+    camera.position.z = 5;
+    scene.add(camera);
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+    // Controls
+    // @ts-ignore
+    const controls = new OrbitControls(camera, canvas);
+    controls.enableDamping = true;
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
-
-    // Update renderer
+    /**
+     * Renderer
+     */
+    const renderer = new THREE.WebGLRenderer({
+        canvas: canvas,
+    });
+    renderer.outputColorSpace = THREE.LinearSRGBColorSpace;
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+    renderer.setClearColor('#262837')
+    /**
+     * Animate
+     */
+    const clock = new THREE.Clock();
 
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 4
-camera.position.y = 2
-camera.position.z = 5
-scene.add(camera)
+    const tick = () => {
+        const elapsedTime = clock.getElapsedTime();
 
-// Controls
-// @ts-ignore
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
+        // Update controls
+        controls.update();
 
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.outputColorSpace = THREE.LinearSRGBColorSpace
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+        // Render
+        renderer.render(scene, camera);
 
-/**
- * Animate
- */
-const clock = new THREE.Clock()
+        // Call tick again on the next frame
+        window.requestAnimationFrame(tick);
+    };
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
-
-    // Update controls
-    controls.update()
-
-    // Render
-    renderer.render(scene, camera)
-
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
-
-tick()
+    tick();
 </script>
 
 <main />
